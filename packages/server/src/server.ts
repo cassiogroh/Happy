9705d00b21f5e55f7ad035f350857dd0dedcc8eb
cassiogroh <cassio_groh@hotmail.com>
2020-10-13 16:41:43 -0300

    chore: Created API

---
+++ packages/server/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import 'express-async-errors';
+import cors from 'cors';
+
+import './database/connection';
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+const app = express();
+
+app.use(cors({
+  origin: `${process.env.WEB_URL}`
+}));
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Happy server running on port ${port}`));
