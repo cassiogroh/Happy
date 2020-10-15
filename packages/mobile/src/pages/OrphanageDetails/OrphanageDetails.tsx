a3d281e0b4c69d1b917c84849e1cffa3eb9a5fde
cassiogroh <cassio_groh@hotmail.com>
2020-10-15 17:58:45 -0300

    chore: Create mobile app

---
+++ packages/mobile/src/pages/OrphanageDetails/OrphanageDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+const OrphanageDetails: React.FC = () => {
+  return (
+    <View>
+      <Text>Details screen</Text>
+    </View>
+  )
+}
+
+export default OrphanageDetails;
